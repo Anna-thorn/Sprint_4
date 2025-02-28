11ff2c1ea58bedeb19f03452b9b084c2dbf5adee
Anna Stolyarova <mushkada12@yandex.ru>
2025-02-28 15:21:28 +0300

    Переименование класса

---
+++ src/main/java/mainPage/HeaderPage.java
@@ -0,0 +1,29 @@
+package mainPage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HeaderPage {
+    private final WebDriver driver;
+    // конструктор
+    public HeaderPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // локаторы
+    // изменено - кнопка "Заказать" в верхней части страницы
+    private final By orderButtonTop = By.xpath("//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']");
+
+    // новое - кнопка "Заказать" в середине страницы
+    private final By orderButtonMiddle = By.xpath("//button[contains(@class, 'Button_Button__ra12g') and contains(@class, 'Button_Middle__1CSJM') and text()='Заказать']");
+
+    // методы
+    // клик по кнопке "Заказать" в верхней части страницы
+    public void clickOrderButtonTop() {
+        driver.findElement(orderButtonTop).click();
+    }
+    // новое - клик по кнопке "Заказать" в середине страницы
+    public void clickOrderButtonMiddle() {
+        driver.findElement(orderButtonMiddle).click();
+    }
+}
